403f763a7063de381edd3187d5dc0595d449d331
UrielHeredia <urito100he@gmail.com>
2023-09-05 09:59:01 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/tp6/TP6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tp6;
+
+/**
+ *
+ * @author Uriel
+ */
+public class TP6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
